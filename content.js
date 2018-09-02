b05e4018558cfa6ecd2068599c15b9879bbd7061
LorieFice <thib.magnin@orange.fr>
2018-09-02 21:52:34 +0200

    ça fonctionne c'est le principal xd

---
+++ content.js
@@ -0,0 +1,4 @@
+var acceuil = document.getElementsByClassName('style-scope ytd-page-manager');
+var file = document.getElementById('related');
+console.log(file);
+acceuil[0].parentNode.removeChild(acceuil[0]);
